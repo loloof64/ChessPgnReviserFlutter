19a261b21532133142c40063a885b46ff75aba60
loloof64 <laurent.bernabe@gmail.com>
2021-05-20 11:03:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chess_pgn_reviser
+
+Load your chess PGN file and try to find moves (from your side).
---
+++ android/app/src/main/kotlin/com/example/chess_pgn_reviser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chess_pgn_reviser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
