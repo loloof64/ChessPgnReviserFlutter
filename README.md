d183bcd3db559f6a9eb0611b1af5bf393fbe832a
loloof64 <laurent.bernabe@gmail.com>
2021-05-20 15:32:55 +0200

    We can choose the promotion piece.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # chess_pgn_reviser
 
 Load your chess PGN file and try to find moves (from your side).
+
+## Credits
+
+Using some pictures from flaticon.com:
+* red_cross : downloaded at https://www.flaticon.com/free-icon/cancel_1168643 and designed by FreePik.
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:chess/chess.dart' as board_logic;
-import 'package:flutter_stateless_chessboard/flutter_stateless_chessboard.dart' as board;
+import 'package:flutter_stateless_chessboard/flutter_stateless_chessboard.dart'
+    as board;
+import 'package:chess_vectors_flutter/chess_vectors_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,7 +29,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Chess Pgn Reviser'),
+      home: MyHomePage(title: 'Game page'),
     );
   }
 }
@@ -51,39 +53,196 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _boardState = board_logic.Chess();
+  var _pendingPromotion = false;
+  board.ShortMove _pendingPromotionMove;
 
-  board_logic.Chess boardState = board_logic.Chess();
+  checkAndMakeMove(board.ShortMove move) {
+    var boardLogicClone = board_logic.Chess();
+    boardLogicClone.load(_boardState.fen);
+    final legalMove = boardLogicClone
+        .move({'from': move.from, 'to': move.to, 'promotion': 'q'});
+    if (legalMove) {
+      final isPawn =
+          _boardState.get(move.from).type == board_logic.PieceType.PAWN;
+      final isWhitePromotion = _boardState.turn == board_logic.Color.WHITE &&
+          move.from.characters.elementAt(1) == "7" &&
+          move.to.characters.elementAt(1) == "8";
+      final isBlackPromotion = _boardState.turn == board_logic.Color.BLACK &&
+          move.from.characters.elementAt(1) == "2" &&
+          move.to.characters.elementAt(1) == "1";
+      final isPromotion = isPawn && (isWhitePromotion || isBlackPromotion);
 
-  makeMove(board.ShortMove move, String promotion) {
-    final moveDone = boardState.move({'from': move.from, 'to': move.to, 'promotion': 'q'});
-    if (moveDone) {
-      // We need to notify that state has been updated.
-      setState(() {
-        
-      });
+      if (isPromotion) {
+        setState(() {
+          _pendingPromotion = true;
+          _pendingPromotionMove = move;
+        });
+      } else {
+        _boardState.move({'from': move.from, 'to': move.to});
+        setState(() {
+          // We need to notify that state has been updated.
+          // Nothing to add here.
+        });
+      }
     }
   }
 
+  commitPromotionMove(String type) {
+    _boardState.move({
+      'from': _pendingPromotionMove.from,
+      'to': _pendingPromotionMove.to,
+      'promotion': type
+    });
+    setState(() {
+      // We need to notify that state has been updated.
+      // Nothing to add here.
+    });
+    cancelPendingPromotion();
+  }
+
+  cancelPendingPromotion() {
+    setState(() {
+      _pendingPromotionMove = null;
+      _pendingPromotion = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final viewport = MediaQuery.of(context).size;
     final size = min(viewport.height * 0.8, viewport.width);
+    final promotionPieceSize = size / 7.0;
+
+    var children = <Widget>[
+      board.Chessboard(
+          fen: _boardState.fen,
+          size: size,
+          onMove: (move) {
+            checkAndMakeMove(move);
+          })
+    ];
+
+    if (_pendingPromotion) {
+      List<Widget> promotionButtons;
+      if (_boardState.turn == board_logic.Color.WHITE) {
+        promotionButtons = <Widget>[
+          TextButton.icon(
+            label: Text(''),
+            icon: WhiteQueen(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('q');
+            },
+          ),
+          TextButton.icon(
+            label: Text(''),
+            icon: WhiteRook(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('r');
+            },
+          ),
+          TextButton.icon(
+            label: Text(''),
+            icon: WhiteBishop(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('b');
+            },
+          ),
+          TextButton.icon(
+            label: Text(''),
+            icon: WhiteKnight(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('n');
+            },
+          ),
+        ];
+      } else {
+        promotionButtons = <Widget>[
+          TextButton.icon(
+            label: Text(''),
+            icon: BlackQueen(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('q');
+            },
+          ),
+          TextButton.icon(
+            label: Text(''),
+            icon: BlackRook(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('r');
+            },
+          ),
+          TextButton.icon(
+            label: Text(''),
+            icon: BlackBishop(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('b');
+            },
+          ),
+          TextButton.icon(
+            label: Text(''),
+            icon: BlackKnight(
+              size: promotionPieceSize,
+            ),
+            onPressed: () {
+              commitPromotionMove('n');
+            },
+          ),
+        ];
+      }
+      children.insert(
+          1,
+          Opacity(
+            opacity: 0.3,
+            child: Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ...promotionButtons,
+                  TextButton.icon(
+                    label: Text(''),
+                    icon: Image(
+                      image: AssetImage('images/red_cross.png'),
+                      width: promotionPieceSize,
+                      height: promotionPieceSize,
+                    ),
+                    onPressed: () {
+                      cancelPendingPromotion();
+                    },
+                  ),
+                ],
+              ),
+              width: size,
+              height: size,
+              color: Colors.blue[600],
+            ),
+          ));
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            board.Chessboard(fen: boardState.fen, size: size, onMove: (move) {
-              makeMove(move, 'q');
-            })
-                        ],
-                      ),
-                    ),
-                  );
-                }
+        child: Stack(
+          children: children,
+        ),
+      ),
+    );
+  }
 }
