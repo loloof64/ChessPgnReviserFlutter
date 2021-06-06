27aa0e87770c05f8dc96de631cb8f31b13fb1612
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-06-06 19:56:17 +0200

    Checking that the position is not illegal and that we can make move, when loading a game.

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -72,6 +72,15 @@ class _GamePageState extends State<GamePage> {
       final game = allGames[gameIndex];
       final fen = (game["tags"] ?? {})["FEN"] ?? board_logic.Chess().fen;
 
+      final gameLogic = board_logic.Chess.fromFEN(fen);
+      _checkPiecesCount(gameLogic);
+      final moves = gameLogic.generate_moves();
+      final noMoreMove = moves.isEmpty;
+
+      if (noMoreMove) {
+        throw Exception("Cannot load the position : no move can be made !");
+      }
+
       setState(() {
         _boardState = board_logic.Chess.fromFEN(fen);
         _boardReversed = fen.split(" ")[1] == "b";
@@ -82,6 +91,98 @@ class _GamePageState extends State<GamePage> {
       Toast.show("Failed to read pgn content, cancelled new game !", context,
           duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
     }
+  }
+
+  String _pieceTypeToFen(board_logic.Piece pieceType) {
+    if (pieceType.type == board_logic.PieceType.PAWN &&
+        pieceType.color == board_logic.Color.WHITE) return 'P';
+    if (pieceType.type == board_logic.PieceType.PAWN &&
+        pieceType.color == board_logic.Color.BLACK) return 'p';
+    if (pieceType.type == board_logic.PieceType.KNIGHT &&
+        pieceType.color == board_logic.Color.WHITE) return 'N';
+    if (pieceType.type == board_logic.PieceType.KNIGHT &&
+        pieceType.color == board_logic.Color.BLACK) return 'n';
+    if (pieceType.type == board_logic.PieceType.BISHOP &&
+        pieceType.color == board_logic.Color.WHITE) return 'B';
+    if (pieceType.type == board_logic.PieceType.BISHOP &&
+        pieceType.color == board_logic.Color.BLACK) return 'b';
+    if (pieceType.type == board_logic.PieceType.ROOK &&
+        pieceType.color == board_logic.Color.WHITE) return 'R';
+    if (pieceType.type == board_logic.PieceType.ROOK &&
+        pieceType.color == board_logic.Color.BLACK) return 'r';
+    if (pieceType.type == board_logic.PieceType.QUEEN &&
+        pieceType.color == board_logic.Color.WHITE) return 'Q';
+    if (pieceType.type == board_logic.PieceType.QUEEN &&
+        pieceType.color == board_logic.Color.BLACK) return 'q';
+    if (pieceType.type == board_logic.PieceType.KING &&
+        pieceType.color == board_logic.Color.WHITE) return 'K';
+    if (pieceType.type == board_logic.PieceType.KING &&
+        pieceType.color == board_logic.Color.BLACK) return 'k';
+    return null;
+  }
+
+  _checkPiecesCount(board_logic.Chess gameLogic) {
+    final piecesCounts = Map<String, int>();
+    for (var rank = 0; rank < 8; rank++) {
+      for (var file = 0; file < 8; file++) {
+        final cell = board_logic.Chess.algebraic(16 * rank + file);
+        final currentPiece = gameLogic.get(cell);
+        ///////////////////
+        print("$cell, $currentPiece");
+        //////////////////////
+        if (currentPiece != null) {
+          final currentFen = _pieceTypeToFen(currentPiece);
+          if (piecesCounts.containsKey(currentFen)) {
+            piecesCounts[currentFen] += 1;
+          } else {
+            piecesCounts[currentFen] = 1;
+          }
+        }
+      }
+    }
+
+    ///////////////////////
+    print(piecesCounts);
+    ///////////////////////
+
+    if (!piecesCounts.containsKey('K')) {
+      throw Exception("No white king !");
+    }
+    if (!piecesCounts.containsKey('k')) {
+      throw Exception("No black king !");
+    }
+
+    if (piecesCounts['K'] != 1) {
+      throw Exception("There must be exactly one white king !");
+    }
+    if (piecesCounts['k'] != 1) {
+      throw Exception("There must be exactly one black king !");
+    }
+
+    if (piecesCounts.containsKey('K') && piecesCounts['K'] > 8)
+      throw Exception("Too many white pawns !");
+    if (piecesCounts.containsKey('k') && piecesCounts['k'] > 8)
+      throw Exception("Too many black pawns !");
+
+    if (piecesCounts.containsKey('N') && piecesCounts['N'] > 10)
+      throw Exception("Too many white knights !");
+    if (piecesCounts.containsKey('n') && piecesCounts['n'] > 10)
+      throw Exception("Too many black knights !");
+
+    if (piecesCounts.containsKey('B') && piecesCounts['B'] > 10)
+      throw Exception("Too many white bishops !");
+    if (piecesCounts.containsKey('b') && piecesCounts['b'] > 10)
+      throw Exception("Too many black bishops !");
+
+    if (piecesCounts.containsKey('R') && piecesCounts['R'] > 10)
+      throw Exception("Too many white rooks !");
+    if (piecesCounts.containsKey('r') && piecesCounts['r'] > 10)
+      throw Exception("Too many black rooks !");
+
+    if (piecesCounts.containsKey('Q') && piecesCounts['Q'] > 9)
+      throw Exception("Too many white queens !");
+    if (piecesCounts.containsKey('q') && piecesCounts['q'] > 9)
+      throw Exception("Too many black queens !");
   }
 
   startNewGame(BuildContext context) {
