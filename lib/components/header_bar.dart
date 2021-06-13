a87f80039f303ac0064c850666ccc509159e4a25
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-06-13 10:52:23 +0200

    Refactoring #6

---
+++ lib/components/header_bar.dart
@@ -0,0 +1,89 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+
+class HeaderBar extends StatelessWidget {
+  final double width;
+  final double height;
+
+  final Function startGame;
+  final Function reverseBoard;
+  final Function stopGame;
+
+  HeaderBar({
+    @required this.width,
+    @required this.height,
+    @required this.startGame,
+    @required this.stopGame,
+    @required this.reverseBoard,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final commonHeight = height * 0.9;
+    final commonPadding = 10.0;
+
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        color: Colors.teal[200],
+      ),
+      margin: EdgeInsets.all(8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          HeaderBarButton(
+            imageReference: 'images/racing_flag.png',
+            imagePadding: commonPadding,
+            imageHeight: commonHeight,
+            onPressed: startGame,
+          ),
+          HeaderBarButton(
+            imageReference: 'images/stop.png',
+            imagePadding: commonPadding,
+            imageHeight: commonHeight,
+            onPressed: stopGame,
+          ),
+          HeaderBarButton(
+            imageReference: 'images/reverse_arrows.png',
+            imagePadding: commonPadding,
+            imageHeight: commonHeight,
+            onPressed: reverseBoard,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderBarButton extends StatelessWidget {
+  const HeaderBarButton({
+    @required this.imagePadding,
+    @required this.imageHeight,
+    @required this.onPressed,
+    @required this.imageReference,
+  });
+
+  final double imagePadding;
+  final double imageHeight;
+  final Function onPressed;
+  final String imageReference;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: imagePadding),
+      child: TextButton.icon(
+        label: Text(''),
+        icon: Image(
+          image: AssetImage(imageReference),
+          width: imageHeight,
+          height: imageHeight,
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -13,6 +13,7 @@ import '../components/chessboard/chessboard.dart' as board;
 import '../components/chessboard/chessboard_types.dart';
 import '../components/history.dart';
 import '../utils/chess_utils.dart' as chess_utils;
+import '../components/header_bar.dart';
 
 const EMPTY_BOARD = "8/8/8/8/8/8/8/8 w - - 0 1";
 
@@ -374,93 +375,6 @@ class GoalLabel extends StatelessWidget {
     return Text(
       goalString,
       style: TextStyle(fontSize: minSize * 0.04),
-    );
-  }
-}
-
-class HeaderBar extends StatelessWidget {
-  final double width;
-  final double height;
-
-  final Function startGame;
-  final Function reverseBoard;
-  final Function stopGame;
-
-  HeaderBar({
-    @required this.width,
-    @required this.height,
-    @required this.startGame,
-    @required this.stopGame,
-    @required this.reverseBoard,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final commonHeight = height * 0.9;
-    final commonPadding = 10.0;
-
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8.0)),
-        color: Colors.teal[200],
-      ),
-      margin: EdgeInsets.all(8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          HeaderBarButton(
-            imageReference: 'images/racing_flag.png',
-            imagePadding: commonPadding,
-            imageHeight: commonHeight,
-            onPressed: startGame,
-          ),
-          HeaderBarButton(
-            imageReference: 'images/stop.png',
-            imagePadding: commonPadding,
-            imageHeight: commonHeight,
-            onPressed: stopGame,
-          ),
-          HeaderBarButton(
-            imageReference: 'images/reverse_arrows.png',
-            imagePadding: commonPadding,
-            imageHeight: commonHeight,
-            onPressed: reverseBoard,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class HeaderBarButton extends StatelessWidget {
-  const HeaderBarButton({
-    @required this.imagePadding,
-    @required this.imageHeight,
-    @required this.onPressed,
-    @required this.imageReference,
-  });
-
-  final double imagePadding;
-  final double imageHeight;
-  final Function onPressed;
-  final String imageReference;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: imagePadding),
-      child: TextButton.icon(
-        label: Text(''),
-        icon: Image(
-          image: AssetImage(imageReference),
-          width: imageHeight,
-          height: imageHeight,
-        ),
-        onPressed: onPressed,
-      ),
     );
   }
 }
